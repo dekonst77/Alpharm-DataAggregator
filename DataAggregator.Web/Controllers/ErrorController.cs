56fc5db12e27c50db72fb50c01c617a9ed921c1d
k.derepaskin <k.derepaskin@alpharm.ru>
2022-11-08 11:09:05 +0300

    Response.StatusCode

--- DataAggregator.Web/Controllers/ErrorController.cs
+++ DataAggregator.Web/Controllers/ErrorController.cs
@@ -1,24 +1,37 @@
 namespace DataAggregator.Web.Controllers
 {
     using System;
+    using System.Net;
+    using System.Web;
     using System.Web.Mvc;
 
     public class ErrorController : Controller
     {
         public ActionResult Unauthorized()
         {
-            Response.StatusCode = 401;
+            Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+
+            ViewBag.StatusCode = RouteData.Values["StatusCode"];
+            ViewBag.Path = RouteData.Values["Path"];
+            ViewBag.Description = RouteData.Values["StatusDescription"];
+
             return View();
         }
         public ActionResult NotFound()
         {
-            Response.StatusCode = 404;
+            Response.StatusCode = (int)HttpStatusCode.NotFound;
+
+            ViewBag.StatusCode = RouteData.Values["StatusCode"];
+            ViewBag.Path = RouteData.Values["Path"];
+            ViewBag.Description = RouteData.Values["StatusDescription"];
+
             return View();
         }
 
         public ActionResult Forbidden()
         {
-            Response.StatusCode = 403;
+            Response.StatusCode = (int)HttpStatusCode.Forbidden;
+
             return View();
         }
     }
