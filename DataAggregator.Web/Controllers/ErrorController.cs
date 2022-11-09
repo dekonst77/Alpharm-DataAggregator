9f2ec85457bb11f1e22f920a72e87aa7e17acd47
k.derepaskin <k.derepaskin@alpharm.ru>
2022-11-09 14:13:54 +0300

    обработка представлений ошибок

--- DataAggregator.Web/Controllers/ErrorController.cs
+++ DataAggregator.Web/Controllers/ErrorController.cs
@@ -5,6 +5,9 @@ namespace DataAggregator.Web.Controllers
     using System.Web;
     using System.Web.Mvc;
 
+    /// <summary>
+    /// This controller exists to provide the error page
+    /// </summary>
     public class ErrorController : Controller
     {
         private int GetStatusCode(Exception exception)
@@ -13,48 +16,61 @@ namespace DataAggregator.Web.Controllers
             return httpException != null ? httpException.GetHttpCode() : (int)HttpStatusCode.InternalServerError;
         }
 
-        public ActionResult Unauthorized()
+        public ViewResult Unauthorized()
         {
-            Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+            Response.StatusCode = (int)HttpStatusCode.Unauthorized; // 401
 
-            ViewBag.StatusCode = RouteData.Values["StatusCode"];
-            ViewBag.Path = RouteData.Values["Path"];
-            ViewBag.Description = RouteData.Values["StatusDescription"];
+            return View("Unauthorized");
+        }
+        public ViewResult Forbidden()
+        {
+            Response.StatusCode = (int)HttpStatusCode.Forbidden; // 403
 
             return View();
         }
-        public ActionResult NotFound()
+        public ViewResult NotFound()
         {
-            Response.StatusCode = (int)HttpStatusCode.NotFound;
+            Response.StatusCode = (int)HttpStatusCode.NotFound; // 404
 
-            ViewBag.StatusCode = RouteData.Values["StatusCode"];
-            ViewBag.Path = RouteData.Values["Path"];
-            ViewBag.Description = RouteData.Values["StatusDescription"];
+            return View("NotFound");
+        }
+        public ViewResult InternalServerError()
+        {
+            Response.StatusCode = (int)HttpStatusCode.InternalServerError; // 500
 
-            return View();
+            return View("InternalServerError");
         }
 
-        public ActionResult Forbidden()
-        {
-            Response.StatusCode = (int)HttpStatusCode.Forbidden;
-
-            return View();
-        }
-
-        public ActionResult InternalServerError()
+        /// <summary>
+        /// Обработка ошибок http
+        /// </summary>
+        /// <returns></returns>
+        public ViewResult HttpError()
         {
             Exception exception = (Exception)RouteData.Values["error"];
 
             Response.StatusCode = GetStatusCode(exception);
 
-            return View();
+            ViewBag.StatusCode = Response.StatusCode;
+
+            // представление всех остальных кодов HTTP
+            return View("HttpError");
         }
 
-        public ActionResult General()
+        /// <summary>
+        /// Обработка ошибок сервера
+        /// </summary>
+        /// <returns></returns>
+        public ViewResult General()
         {
-            Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            Exception exception = (Exception)RouteData.Values["error"];
 
-            return View();
+            Response.StatusCode = GetStatusCode(exception);
+
+            ViewBag.StatusCode = Response.StatusCode;
+
+            // представление по умолчанию
+            return View("General");
         }
 
     }
