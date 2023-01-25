4da08edb48025dbeebc9513f5b0ac5fc479b4755
a.kukoba <a.kukoba@alpharm.ru>
2023-01-25 14:43:19 +0300

    #HD10835 обновление приоритетных слов

--- DataAggregator.Web/Scripts/Systematization/PrioritetWordsController.js
+++ DataAggregator.Web/Scripts/Systematization/PrioritetWordsController.js
@@ -23,8 +23,12 @@ function PrioritetWordsController($scope, $route, $http, $uibModal, commonServic
                 editDropdownIdLabel: 'Id', editDropdownValueLabel: 'Name'
             },
             { enableCellEdit: true,name: 'Name', field: 'Name', filter: { condition: uiGridCustomService.condition } },
-            { enableCellEdit: true,headerTooltip: true, name: 'Value', field: 'Value', filter: { condition: uiGridCustomService.condition } }
- 
+            { enableCellEdit: true,headerTooltip: true, name: 'Value', field: 'Value', filter: { condition: uiGridCustomService.condition } },
+            {
+                enableCellEdit: false, name: 'Обработка очереди',
+                cellTemplate: '<div class="ui-grid-cell-contents"><span ng-if="row.entity.Queuing_Order == null" title="Дата начала: {{row.entity.DtStart|date:\'dd.MM.yyyy HH:mm:ss\'}} Дата завершения: {{row.entity.DtEnd|date:\'dd.MM.yyyy HH:mm:ss\'}}">{{row.entity.StatusDescription}}</span><span ng-if="row.entity.Queuing_Order != null" title="Номер в очереди: {{row.entity.Queuing_Order}}">{{row.entity.StatusQueue}}</small></div>',
+                filter: { condition: uiGridCustomService.condition }
+            },
         ];
 
         $scope.dataLoading = $http({
@@ -32,10 +36,10 @@ function PrioritetWordsController($scope, $route, $http, $uibModal, commonServic
             url: '/PrioritetWords/Init/',
             data: JSON.stringify({})
         }).then(function (response) {
+          
             var data = response.data;
 
             Array.prototype.push.apply($scope.Source, response.data.Data.Source);
-
             $scope.Search();
             return response.data;
         });
@@ -46,11 +50,13 @@ function PrioritetWordsController($scope, $route, $http, $uibModal, commonServic
             $scope.CurrentRow = row;
         }
     }
+
     textChange = function () {
         if ($scope.CurrentRow !== undefined) {
             $scope.Grid_PrioritetWords.GridCellsMod($scope.CurrentRow, "@modify", true);
         }
     };
+
     $scope.Search_AC = function () {
         $scope.dataLoading =
             $http({
@@ -58,14 +64,21 @@ function PrioritetWordsController($scope, $route, $http, $uibModal, commonServic
                 url: '/PrioritetWords/PrioritetWords_search/',
                 data: JSON.stringify({})
             }).then(function (response) {
+          
                 var data = response.data;
                 if (data.Success) {
                     $scope.Grid_PrioritetWords.Options.data = data.Data.PrioritetWords;
+                    
+                    if ($scope.Grid_PrioritetWords.gridApi.grid.columns[1].filters[0].term !== undefined) {
+                        var source = $scope.Source.filter(function (item, index) { return item.Name == $scope.Grid_PrioritetWords.gridApi.grid.columns[1].filters[0].term })[0];
+                        $scope.SetFilter(source.Id, source.Name);
+                    }
                 }
             }, function (response) {
                 errorHandlerService.showResponseError(response);
             });
     };
+
     $scope.Search = function () {
         if ($scope.Grid_PrioritetWords.NeedSave === true) {
             messageBoxService.showConfirm('Есть несохранёные результаты. Сохранить?', 'Изменение')
@@ -88,6 +101,7 @@ function PrioritetWordsController($scope, $route, $http, $uibModal, commonServic
         }
 
     };
+
     $scope.Save = function (action) {
         $scope.dataLoading =
             $http({
@@ -113,6 +127,7 @@ function PrioritetWordsController($scope, $route, $http, $uibModal, commonServic
                 errorHandlerService.showResponseError(response);
             });
     };
+
     $scope.Add = function () {
         var new_element = {
             Id : -1,
@@ -122,10 +137,21 @@ function PrioritetWordsController($scope, $route, $http, $uibModal, commonServic
         };
         $scope.Grid_PrioritetWords.Options.data.push(new_element);
     };
+
     $scope.SetFilter = function (Id,Name) {
         $scope.CurrentSourceId = Id;
         $scope.Grid_PrioritetWords.gridApi.grid.columns[1].filters[0].term = Name;
-
+        var sourceId = $scope.Source.filter(function (item, index) { return item.Name == $scope.Grid_PrioritetWords.gridApi.grid.columns[1].filters[0].term })[0].Id;
+        var data = $scope.Grid_PrioritetWords.Options.data.filter(function (item, index) { return item.SourceId == sourceId });
+        if (data.length > 0) {
+            $scope.Grid_PrioritetWords.gridApi.grid.modifyRows(data);
+            $timeout(function () {
+                $scope.Grid_PrioritetWords.gridApi.selection.selectRow($scope.Grid_PrioritetWords.gridApi.core.getVisibleRows()[0].entity);
+            });
+        }
+        else {
+            $scope.CurrentRow = {};
+        }
     };
 
 }
