94b59ddfe5d7f38a9b86a4ba4bce2d2be0171f60
Konstantin Derepaskin <k.derepaskin@alpharm.ru>
2023-10-20 17:21:40 +0300

    добавить клиентскую валидацию при регистрации пользователя

--- DataAggregator.Web/Models/ViewModels/Account/RegisterViewModel.cs
+++ DataAggregator.Web/Models/ViewModels/Account/RegisterViewModel.cs
@@ -4,11 +4,11 @@ namespace DataAggregator.Web.Models
 {
     public sealed class RegisterViewModel
     {
-        [Required]
         [Display(Name = "Логин")]
+        [Required(ErrorMessage = "Введите логин")]
         public string Name { get; set; }
         
-        [Required]
+        [Required(ErrorMessage = "Введите фамилию")]
         [Display(Name = "Фамилия")]
         public string Surname { get; set; }
 
@@ -18,12 +18,12 @@ namespace DataAggregator.Web.Models
         [Display(Name = "Отчество")]
         public string MiddleName { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Введите email")]
         [Display(Name = "Email")]
         [EmailAddress(ErrorMessage = "Некорректный адрес электронной почты")]
         public string Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Введите пароль")]
         [DataType(DataType.Password)]
         [Display(Name = "Пароль")]
         public string Password { get; set; }
